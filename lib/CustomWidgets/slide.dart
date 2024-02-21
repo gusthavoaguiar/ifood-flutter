e2378f7f8ebdc6e4a6f0c672fa2e6fd399923c25
unknown <senai@DSN1191109174.edu119.local>
2024-02-21 11:09:58 -0300

    update

--- lib/CustomWidgets/slide.dart
+++ lib/CustomWidgets/slide.dart
@@ -10,6 +10,8 @@ class Slide extends StatefulWidget {
 
 class _SlideState extends State<Slide> {
   PageController pageController = PageController();
+  List<double> progress = [0.0, 0.0, 0.0,];
+  int qtdPages = 3;
   int currentPage = 0;
 
   @override
@@ -17,47 +19,92 @@ class _SlideState extends State<Slide> {
     super.initState();
     nextPage();
   }
-  nextPage(){
+
+  nextPage() {
     Timer.periodic(Duration(seconds: 3), (timer) {
-      int page = pageController.page!.round() + 1;
-      if (page >= 3){
-        pageController.animateToPage(0, duration: Duration(seconds: 2), curve: Curves.easeIn);
+      int page = pageController.page!.round();
+      setState(() {
+        currentPage = page;
+      });
+      if (page >= 3) {
+        pageController.animateToPage(0,
+            duration: Duration(seconds: 2), curve: Curves.easeIn);
       } else {
-        pageController.nextPage(duration: Duration(seconds: 2), curve: Curves.easeIn);
+        pageController.nextPage(
+            duration: Duration(seconds: 2), curve: Curves.easeIn);
       }
-     });
+    });
   }
-  
+
+void startProgress(){
+  Timer.periodic(const Duration(milliseconds: 50), (timer) {
+    setState(() {
+      if (progress[currentPage] < 1) {
+        progress[currentPage] += 0.02;
+      } else {
+        timer.cancel();
+      }
+    });
+   });
+}
+//método para resetar a animação
+void reset(){
+  for(int i = 0; i < qtdPages; i++){
+    progress[i] = 0.0;
+  }
+  startProgress();
+}
+
+//método para criar o indicator
+List<Widget> buildIndicator(){
+  List<Widget> list = [];
+for(int i = 0; i < qtdPages; i++){
+  list.add(Container(
+    width: 50,
+    height: 5,
+    margin: EdgeInsets.all(8),
+    child: LinearProgressIndicator(
+      borderRadius: BorderRadius.circular(8),
+      value: progress[i],
+      backgroundColor: Colors.grey[200],
+      valueColor: AlwaysStoppedAnimation<Color>(currentPage == i? Colors.blue :Colors.grey),
+    ),
+  ));
+}
+return list;
+}
+
   @override
   Widget build(BuildContext context) {
-    
-    return  Container(
-      margin: const EdgeInsets.all(14),
-      width: double.infinity,
-      height: 200,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8)
-      ),
-      child: PageView(
-        controller: pageController,
-        children: [
-          Container(
-            width: double.infinity,
-            height: 200,
-            color: Colors.orange,
+    return Container(
+        margin: const EdgeInsets.all(14),
+        width: double.infinity,
+        height: 200,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
+        child: Stack(
+          alignment: AlignmentDirectional.bottomCenter,
+          children: [
+            PageView(
+              controller: pageController,
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 200,
+                  color: Colors.orange,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 200,
+                  color: Colors.black,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 200,
+                  color: Colors.purple,
+                ),
+              ],
             ),
-            Container(
-            width: double.infinity,
-            height: 200,
-            color: Colors.black,
-            ),
-            Container(
-            width: double.infinity,
-            height: 200,
-            color: Colors.purple,
-            ),
-        ],
-      ),
-    );
+          ],
+        ));
   }
 }
